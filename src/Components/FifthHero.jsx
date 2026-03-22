03b067b61d9321c8141bff7ff980c05ac4756af8
unknown <clintoncute04@gmail.com>
2026-03-22 20:00:31 +0100

    Components

---
+++ src/Components/FifthHero.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './FifithHero.css'
+const FifthHero = () => {
+  return (
+    <>
+      <main className='cont18'>
+        <section>
+            <p className='text11'>What sets Asana apart</p>
+
+            <nav className='cont19'>
+                <div className='cont16'>
+                    <div className='cont17'>
+                <p className='text12'>Amplify your impact with AI</p>
+                <h3 className='text13'>Let Asana AI handle work for you—with <br/>the full context of your business—so your <br/>teams can achieve their goals faster.</h3>
+                </div>
+                 <img className='img7' src="/src/assets/page2.webp" alt="" />
+            </div>
+
+            <div className='cont20'>
+                <p className='text14'>More clarity and accountability</p>
+                <p className='text15'>Connect strategic goals to the teams that<br/> help achieve them. Keep your company<br/> on track with AI working alongside your<br/> teams.</p>
+            </div>
+            </nav>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default FifthHero
