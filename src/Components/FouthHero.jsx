f4245ddd334bf598371d2d74b309502155f4deb9
unknown <clintoncute04@gmail.com>
2026-03-22 03:32:33 +0100

    heros and layout of the landing page

---
+++ src/Components/FouthHero.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import './FouthHero.css'
+const FouthHero = () => {
+  return (
+    <>
+      <main className='cont14'>
+        <p className='text11'>See how Asana keeps<br/> work moving across use<br/> cases</p>
+
+        <nav className='cont15'>
+        <section className='cont13'>
+            <img className='img6' src="/src/assets/tab1.webp" alt="" />
+            <h3 className='text8'>Campiagin<br/> Management</h3>
+
+            <h3 className='text9'>Plan,Track and complete your campaigns all in one place</h3>
+
+            <h3 className='text10'>See campaign management</h3>
+          
+        </section>
+
+
+
+
+
+        <section className='cont13'>
+            <img className='img6' src="/src/assets/tab2.webp" alt="" />
+            <h3 className='text8'>Campiagin<br/> Management</h3>
+
+            <h3 className='text9'>Plan,Track and complete your campaigns all in one place</h3>
+
+            <h3 className='text10'>See campaign management</h3>
+          
+        </section>
+
+
+
+        <section className='cont13'>
+            <img className='img6' src="/src/assets/tab3.webp" alt="" />
+            <h3 className='text8'>Campiagin<br/> Management</h3>
+
+            <h3 className='text9'>Plan,Track and complete your campaigns all in one place</h3>
+
+            <h3 className='text10'>See campaign management</h3>
+          
+        </section>
+
+
+
+
+        <section className='cont13'>
+            <img className='img6' src="/src/assets/tab4.webp" alt="" />
+            <h3 className='text8'>Campiagin<br/> Management</h3>
+
+            <h3 className='text9'>Plan,Track and complete your campaigns all in one place</h3>
+
+            <h3 className='text10'>See campaign management</h3>
+          
+        </section>
+
+
+
+
+        <section className='cont13'>
+            <img className='img6' src="/src/assets/tab5.webp" alt="" />
+            <h3 className='text8'>Campiagin<br/> Management</h3>
+
+            <h3 className='text9'>Plan,Track and complete your campaigns all in one place</h3>
+
+            <h3 className='text10'>See campaign management</h3>
+          
+        </section>
+
+        </nav>
+      </main>
+    </>
+  )
+}
+
+export default FouthHero
