1048ef8d96df313587f6ea03c237accf52c7ea38
unknown <clintoncute04@gmail.com>
2026-03-25 09:02:06 +0100

    additional Hero sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import FifthHero from "./Components/FifthHero"
+import FooterHero from "./Components/FooterHero"
 import FouthHero from "./Components/FouthHero"
 import Header from "./Components/Header"
 import Hero from "./Components/Hero"
 import SecondHero from "./Components/SecondHero"
+import SeventhHero from "./Components/SeventhHero"
 import SixthHero from "./Components/SixthHero"
 import ThirldHero from "./Components/ThirldHero"
 
@@ -19,6 +21,8 @@ function App() {
     <FouthHero/>
     <FifthHero/>
     <SixthHero/>
+    <SeventhHero/>
+    <FooterHero/>
       </>
               
            
